b02c46ec4b8f9f93779aacdd94405e60ee50562d
Aadita Sah <aadityasah13@gmail.com>
2024-10-15 14:47:54 +0530

    minox bugs fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer  = () => {
     <div className='h-72 bg-slate-600'>
         <div className=' flex justify-around text-white p-5'>
             <div className='w-1/3 md:w-2/4'>
-                    <h1 className='text-3xl sm:text-5xl'>Work Manager</h1>
+                    <h1 className='text-2xl sm:text-5xl'>Work Manager</h1>
                     <p className='mt-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit.<span className='hidden md:block'> Voluptate possimus ipsa fuga neque fugit error hic in! Minus voluptate aperiam iusto numquam! Totam sunt error quia minus delectus illum quos?</span></p>
             </div>
             <div className='w-2/3 md:w-2/4 flex justify-around'>
